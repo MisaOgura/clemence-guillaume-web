96b7e43621523e5496b396c6dbdd8d535473f43a
MisaOgura <misa.ogura01@gmail.com>
2016-05-29 17:31:50 +0100

    Add /users route

--- server.js
+++ server.js
@@ -8,8 +8,21 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({"extended" : false}));
 
 router.get("/",function(req,res){
-    res.json({"error" : false,"message" : "Hello World"});
+  res.json({"error" : false,"message" : "Hello World"});
 });
+
+router.route("/users")
+  .get(function(req,res){
+    var response = {};
+    mongoOp.find({},function(err,data){
+      if(err) {
+        response = {"error" : true,"message" : "Error fetching data"};
+      } else {
+        response = {"error" : false,"message" : data};
+      }
+      res.json(response);
+    });
+  });
 
 app.use('/',router);
 
