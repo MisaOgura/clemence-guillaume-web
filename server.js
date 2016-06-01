91d7815998217a16a6d3b1d192795b662ef23f0a
MisaOgura <misa.ogura01@gmail.com>
2016-06-01 13:20:24 +0100

    Implement get request with user_id and put request to update email and password

--- server.js
+++ server.js
@@ -41,6 +41,42 @@ router.route("/users")
         });
     });
 
+router.route("/users/:id")
+  .get(function(req,res){
+      var response = {};
+      mongoOp.findById(req.params.id,function(err,data){
+          if(err) {
+              response = {"error" : true,"message" : "Error fetching data"};
+          } else {
+              response = {"error" : false,"message" : data};
+          }
+          res.json(response);
+      });
+  })
+  .put(function(req,res){
+        var response = {};
+        mongoOp.findById(req.params.id,function(err,data){
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                if(req.body.userEmail !== undefined) {
+                    data.userEmail = req.body.userEmail;
+                }
+                if(req.body.userPassword !== undefined) {
+                    data.userPassword = req.body.userPassword;
+                }
+                data.save(function(err){
+                    if(err) {
+                        response = {"error" : true,"message" : "Error updating data"};
+                    } else {
+                        response = {"error" : false,"message" : "Data is updated for "+req.params.id};
+                    }
+                    res.json(response);
+                })
+            }
+        });
+    });
+
 app.use('/',router);
 
 app.listen(3000);
