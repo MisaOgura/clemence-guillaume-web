03fd88aa2f80d2f9720001eda7dfec44577ad922
MisaOgura <misa.ogura01@gmail.com>
2016-06-12 19:42:33 +0100

    Remove unused tests

--- test/e2e/aboutPageFeatures.js
+++ test/e2e/aboutPageFeatures.js
@@ -3,16 +3,4 @@
 //   beforeEach(function() {
 //     browser.get('/about');
 //   });
-//
-//   it('has a title', function() {
-//     expect(browser.getTitle()).toEqual('CG About')
-//   });
-//
-//   it('has a link to the Home page', function() {
-//     linkToHome = element(by.id('home-link'));
-//     homeUrl = 'http://localhost:8080/'
-//     linkToHome.click().then(function() {
-//       expect(browser.getCurrentUrl()).toEqual(homeUrl)
-//     });
-//   });
 // });
--- test/e2e/portfolioPageFeatures.js
+++ test/e2e/portfolioPageFeatures.js
@@ -3,16 +3,4 @@
 //   beforeEach(function() {
 //     browser.get('/portfolio');
 //   });
-//
-//   it('has a title', function() {
-//     expect(browser.getTitle()).toEqual('CG Portfolio')
-//   });
-//
-//   it('has a link to the Home page', function() {
-//     linkToHome = element(by.id('home-link'));
-//     homeUrl = 'http://localhost:8080/'
-//     linkToHome.click().then(function() {
-//       expect(browser.getCurrentUrl()).toEqual(homeUrl)
-//     });
-//   });
 // });
